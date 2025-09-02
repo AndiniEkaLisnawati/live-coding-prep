e0168030e2a98b8f5f8516ca1c57be262aea58d1
AndiniEkaLisnawati <andiniekalisnawatililis2@gmail.com>
2025-09-02 20:10:31 +0700

    finish

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // waktu pngerjaan 40 menit
 // input [1, 4, 6, 7] => [14, 12, 8, 2]
 
+//algoritma:
 //balik array 
+//kali dua
 let input = [1, 4, 6, 7]
 function reverseArr(arr){
     for(let i = 3; i >= 0; i-- ){
         const n = arr[i]
         console.log(n * 2)
-      
     }
  
 }
