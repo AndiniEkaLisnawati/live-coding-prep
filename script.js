e5537ce2245d136acbfcedfc769864b4e3dd690a
AndiniEkaLisnawati <andiniekalisnawatililis2@gmail.com>
2025-09-02 19:51:15 +0700

    commit 10  menit

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // waktu pngerjaan 40 menit
 // input [1, 4, 6, 7] => [14, 12, 8, 2]
 
-for(let i = 0; i < )
+//balik array 
+let input = [1, 4, 6, 7]
+for(let i = 0; i < input.length; i++ ){
+    const n = input[]
+}
